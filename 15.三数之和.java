b2ea15e641858d23e74b6fee2648f8d82db93772
RabbitTank <moondust1196551872@gmail.com>
2025-12-12 21:47:25 +0800

    leetcode20251212

---
+++ 15.三数之和.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=15 lang=java
+ *
+ * [15] 三数之和
+ */
+
+// @lc code=start
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                
+            }
+        }
+
+
+        return res;
+    }
+}
+// @lc code=end
+
